dee9a9b19c98ba386f1264d380df5edcebd6829e
MaxenceMottard <maxence.mottard@worldline.com>
2021-10-23 10:27:40 +0200

    Use Query params instead of body wiht get requests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { SearchParams } from './dto/SearchParams';
 import { SymbolParams } from './dto/SymbolParams';
@@ -9,22 +9,22 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/search')
-  search(@Body() params: SearchParams) {
+  search(@Query() params: SearchParams) {
     return this.appService.search(params);
   }
 
   @Get('/quote')
-  quote(@Body() params: SymbolParams) {
+  quote(@Query() params: SymbolParams) {
     return this.appService.quote(params);
   }
 
   @Get('/intraday')
-  intraday(@Body() params: IntradayParams) {
+  intraday(@Query() params: IntradayParams) {
     return this.appService.intraday(params);
   }
 
   @Get('/monthly')
-  monthly(@Body() params: SymbolParams) {
+  monthly(@Query() params: SymbolParams) {
     return this.appService.monthly(params);
   }
 }
